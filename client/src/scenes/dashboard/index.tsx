e3883649e9a45078d0ad80ab6a352be1b0fda261
Richer Duong <duongricher@gmail.com>
2024-01-09 20:59:10 -0600

    Add media query for medium screens

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { Box, useTheme } from '@mui/material';
+import { Box, useMediaQuery, useTheme } from '@mui/material';
 import React from 'react';
 
 type Props = {};
@@ -17,6 +17,7 @@ const gridTemplate = `
 `
 
 const Dashboard = (props: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
   const { palette } = useTheme();
   return (
     <Box 

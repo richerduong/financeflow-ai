33d320af4103f65b3c63f31e0f3f35815f953244
Richer Duong <duongricher@gmail.com>
2024-01-10 17:45:27 -0600

    Add shadows to boxes and refactor dashboard index.tsx

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,9 +1,9 @@
-import { Box, useMediaQuery, useTheme } from '@mui/material';
-import React from 'react';
+import { Box, useMediaQuery } from "@mui/material";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
-type Props = {};
-
-const gridTemplate = `
+const gridTemplateLargeScreens = `
   "a b c"
   "a b c"
   "a b c"
@@ -14,33 +14,65 @@ const gridTemplate = `
   "g h i"
   "g h j"
   "g h j"
-`
+`;
+const gridTemplateSmallScreens = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+`;
 
-const Dashboard = (props: Props) => {
-  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
-  const { palette } = useTheme();
+const Dashboard = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   return (
-    <Box 
-      width="100%" 
-      height="100%" 
-      display="grid" 
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
       gap="1.5rem"
-      sx={{
-        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-        gridTemplateAreas: gridTemplate,
-      }}
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
     >
-      <Box bgcolor="#fff" gridArea="a"></Box>
-      <Box bgcolor="#fff" gridArea="b"></Box>
-      <Box bgcolor="#fff" gridArea="c"></Box>
-      <Box bgcolor="#fff" gridArea="d"></Box>
-      <Box bgcolor="#fff" gridArea="e"></Box>
-      <Box bgcolor="#fff" gridArea="f"></Box>
-      <Box bgcolor="#fff" gridArea="g"></Box>
-      <Box bgcolor="#fff" gridArea="h"></Box>
-      <Box bgcolor="#fff" gridArea="i"></Box>
-      <Box bgcolor="#fff" gridArea="j"></Box>
+      <Row1 />
+      <Row2 />
+      <Row3 />
     </Box>
   );
 };
